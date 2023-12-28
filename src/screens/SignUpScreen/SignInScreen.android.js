d55fdab7bd507a3a2f2e323405683b65be053389
Abeera Malik <99583531+123-Abeera@users.noreply.github.com>
2023-12-28 19:41:34 +0500

    SignInScreen.android.js

---
+++ src/screens/SignUpScreen/SignInScreen.android.js
@@ -0,0 +1,11 @@
+// SignInScreen.android.js
+
+import React from 'react';
+import SignUpScreen from '../../screens/SignUpScreen';
+
+const SignInScreenAndroid = (props) => {
+ 
+  return <SignUpScreen {...props} />;
+};
+
+export default SignInScreenAndroid;
