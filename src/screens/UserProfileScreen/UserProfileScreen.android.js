568f808aa6a56aca20b2b75ce646e8253fa58660
Abeera Malik <99583531+123-Abeera@users.noreply.github.com>
2023-12-28 19:45:47 +0500

    UserProfileScreen.android.js

---
+++ src/screens/UserProfileScreen/UserProfileScreen.android.js
@@ -0,0 +1,11 @@
+// UserProfileScreen.android.js
+
+import React from 'react';
+import UserProfileScreen from '../../screens/UserProfileScreen';
+
+const UserProfileScreenAndroid = (props) => {
+ 
+  return <UserProfileScreen {...props} />;
+};
+
+export default UserProfileScreenAndroid;
