0675bbfb2682b16dfef3fa8675f6088f8ff65ad3
Abeera Malik <99583531+123-Abeera@users.noreply.github.com>
2023-12-28 19:46:32 +0500

    UserProfileScreen.ios.js

---
+++ src/screens/UserProfileScreen/UserProfileScreen.ios.js
@@ -0,0 +1,11 @@
+// UserProfileScreen.ios.js
+
+import React from 'react';
+import UserProfileScreen from '../../screens/UserProfileScreen';
+
+const UserProfileScreenIOS = (props) => {
+ 
+  return <UserProfileScreen {...props} />;
+};
+
+export default UserProfileScreenIOS;
